f142703af208e0786d7536b5ddaf197256511e7b
Marija <marija.vujica97@gmail.com>
2026-05-25 13:09:15 +0200

    Fix JSON nesting problem

--- src/main/java/com/mashasoftware/onlinebookstore/book/entity/Book.java
+++ src/main/java/com/mashasoftware/onlinebookstore/book/entity/Book.java
@@ -1,11 +1,8 @@
 package com.mashasoftware.onlinebookstore.book.entity;
 
-import com.mashasoftware.onlinebookstore.purchase.entity.Cart;
-import com.mashasoftware.onlinebookstore.purchase.entity.Purchase;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 @Entity
 @Table(name = "books")
@@ -27,12 +24,6 @@ public class Book {
 
     @Column(nullable = false)
     private Integer quantity;
-
-    @ManyToMany(mappedBy = "books")
-    private List<Purchase> purchases;
-
-    @ManyToMany(mappedBy = "books")
-    private List<Cart> carts;
 
     public Book() {
     }
@@ -57,19 +48,4 @@ public class Book {
         return quantity;
     }
 
-    public List<Purchase> getPurchases() {
-        return purchases;
-    }
-
-    public void setPurchases(List<Purchase> purchases) {
-        this.purchases = purchases;
-    }
-
-    public List<Cart> getCarts() {
-        return carts;
-    }
-
-    public void setCarts(List<Cart> carts) {
-        this.carts = carts;
-    }
 }
--- src/main/java/com/mashasoftware/onlinebookstore/book/repository/BookRepository.java
+++ src/main/java/com/mashasoftware/onlinebookstore/book/repository/BookRepository.java
@@ -4,5 +4,5 @@ import com.mashasoftware.onlinebookstore.book.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Integer> {
 }
